a426623857443a8cc57d92cf6694dc455e4f0b5b
Dipu9813 <dipruc06@gmail.com>
2025-01-26 19:23:00 +0545

    Earth in space-completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'jsm/controls/OrbitControls.js';
 import getStarField from './src/getStarfield.js';
+import {getFresnelMat} from './src/getFresnelMat.js';
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -30,16 +31,44 @@ const material = new THREE.MeshStandardMaterial({
 const earthMesh = new THREE.Mesh(geo, material);
 earthGroup.add (earthMesh);
 
+const lightsMat = new  THREE.MeshBasicMaterial({
+    map: loader.load("textures/earthlights1k.jpg"),
+    blending: THREE.AdditiveBlending,
+    transparent:true,
+    opacity: 0.4
+})
+const lightMesh = new THREE.Mesh(geo, lightsMat);
+earthGroup.add(lightMesh);
+
+const glowMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geo, glowMat);
+glowMesh.scale.setScalar=1.003;
+earthGroup.add(glowMesh);
+
 const stars = getStarField({numStars:2000});
 scene.add(stars);
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff);
-scene.add(hemiLight);
+const cloudMat = new THREE.MeshStandardMaterial({
+    map: loader.load("textures/earthcloudmaptrans.jpg"),
+    transparent: true,
+    blending: THREE.AdditiveBlending,
+    opacity:0.2,
+})
+const cloudMesh = new THREE.Mesh(geo, cloudMat);
+cloudMesh.scale.setScalar=1.003;
+earthGroup.add(cloudMesh);
+
+const sunLight = new THREE.DirectionalLight(0xffffff);
+sunLight.position.set(-2,0.5,1.5)
+scene.add(sunLight);
 
 function animate(){
     requestAnimationFrame(animate);
     //earthMesh.rotation.x += 0.001;
     earthMesh.rotation.y += 0.002;
+    lightMesh.rotation.y += 0.002;
+    cloudMesh.rotation.y += 0.002;
+    glowMesh.rotation.y += 0.002;
     renderer.render(scene,camera);
 }
 animate();
